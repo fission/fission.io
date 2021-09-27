7a4ad9838ad689095d94733f11e4f57e0e79330b
Sanket Sudake <sanketsudake@gmail.com>
2021-09-27 12:56:46 +0530

    add version for npm packages
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-npm install -D --save autoprefixer postcss-cli postcss
+# npm install -D --save autoprefixer postcss-cli postcss
+npm install -D --save autoprefixer@9.7.6 postcss-cli@7.1.0
 
 echo "Running hugo"
 hugo
