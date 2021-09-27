0b10302364219a28e82b67cd1a988daafa327f35
Sanket Sudake <sanketsudake@gmail.com>
2021-09-27 13:07:13 +0530

    Dump submodule status
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+echo "Check submodule status"
+git submodule status --recursive
+git submodule foreach --recursive 'git status'
+
+echo "Npm install"
 # npm install -D --save autoprefixer postcss-cli postcss
 npm install -D --save autoprefixer@9.7.6 postcss-cli@7.1.0
 
