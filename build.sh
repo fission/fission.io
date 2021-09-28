c0a155b8baaf33891d4b69db5ae17b39d2aca968
Sanket Sudake <sanketsudake@gmail.com>
2021-09-28 12:52:43 +0530

    Add npm deps
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ git submodule status --recursive
 git submodule foreach --recursive 'git status'
 
 echo "Npm install"
-# npm install -D --save autoprefixer postcss-cli postcss
-npm install -D --save autoprefixer@9.7.6 postcss-cli@7.1.0
+npm install
 
 echo "Running hugo"
 hugo --minify --path-warnings --gc
