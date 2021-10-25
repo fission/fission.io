1ff9b2db668e3452a8b51d1249490ed8bb1ee25e
Aniket Pal <aniketindian8@gmail.com>
2021-10-26 02:18:38 +0530

    Feat:PWA support added with offline support

---
+++ static/sw.js
@@ -0,0 +1,16 @@
+// service worker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll(["./", "./css/styles.css", "./images"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
