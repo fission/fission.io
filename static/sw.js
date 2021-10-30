32a563329d6bffc3939d6aed0d4d73c92b784119
Aniket Pal <aniketindian8@gmail.com>
2021-10-31 01:34:33 +0530

    cache fix

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then((cache) => {
-      return cache.addAll(["./images"]);
+      return cache.addAll(["./"]);
     })
   );
 });
