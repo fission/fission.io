a764c04d2927d4c2865600387c6bc31b073f650e
soharab-ic <156293296+soharab-ic@users.noreply.github.com>
2024-10-04 18:55:33 +0530

    Release notes and docs update for release v1.20.5 (#273)
    * Updated docs and added release notes for release v1.20.5.
    Modified tools/notes.py to make it work with github generated release notes.
    
    Signed-off-by: Md Soharab Ansari <soharab.ansari@infracloud.io>
    
    * Fix misspell
    
    Signed-off-by: Md Soharab Ansari <soharab.ansari@infracloud.io>
    
    ---------
    
    Signed-off-by: Md Soharab Ansari <soharab.ansari@infracloud.io>

--- tools/notes.py
+++ tools/notes.py
@@ -1,3 +1,46 @@
+import requests
+import re
+
+repo_owner = 'fission'
+repo_name = 'fission'
+
+def remove_author_info(input_string):
+    """
+    Remove the 'by @username in' substring from the input string.
+
+    :param input_string: The original string.
+    :return: The modified string without the author information.
+    """
+    # Use regex to match ' by @username in' where username can be alphanumeric and contain dashes
+    modified_string = re.sub(r' by @[a-zA-Z0-9-]+ in', '', input_string)
+
+    # Strip leading and trailing whitespace
+    return modified_string.strip()
+
+def get_merged_pr_commit_sha(repo_owner, repo_name, pr_number):
+    """
+    Get the commit SHA of a merged pull request from a public repository.
+
+    :param repo_owner: The owner of the repository (GitHub username or organization).
+    :param repo_name: The name of the repository.
+    :param pr_number: The number of the pull request.
+    :return: Commit SHA if PR is merged, otherwise None.
+    """
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{pr_number}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pr_data = response.json()
+        if pr_data['merged_at']:  # Check if the PR is merged
+            return pr_data['merge_commit_sha']
+        else:
+            print("Pull request is not merged.")
+    else:
+        print(f"Failed to retrieve PR: {response.status_code} - {response.text}")
+
+    return None
+
 def read_file(file_name):
     """Read content from a file and return the content as a string."""
     with open(file_name, 'r') as f:
@@ -7,15 +50,19 @@ def format_commit_and_pull_link(line):
     """Format the commit and pull link based on the provided line."""
     cols = line.split(' ')
     if len(cols) > 2:
+        pull_number = cols[-1].lstrip('#')
+        commit = get_merged_pr_commit_sha(repo_owner=repo_owner, repo_name=repo_name, pr_number=pull_number)
         # Format commit link
-        cols[1] = "[{0}](https://github.com/fission/fission/commit/{0})".format(cols[1])
+        commit_link = "[{0}](https://github.com/fission/fission/commit/{0})".format(commit[:8])
 
-        # Extract pull number, format and replace in the column
-        pull_number = cols[-1].strip('()')
+        # Put pull number, format and replace in the column
         formatted_pull_link = "[{0}](https://github.com/fission/fission/pull/{1})".format(pull_number, pull_number.replace('#', ''))
         cols[-1] = formatted_pull_link
+        cols = ' '.join(cols)
+        cols = remove_author_info(cols)
 
-        return ' '.join(cols)
+        return '* ' + commit_link + ' ' + cols
+
     return line
 
 def main():
